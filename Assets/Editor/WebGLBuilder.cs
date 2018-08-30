a8c13b7870710e1c059d3d94c842007bbb3bfec3
Nicolas Vanhoren <nicolas.vanhoren@gmail.com>
2018-08-30 18:34:09 +0200

    Added script

---
+++ Assets/Editor/WebGLBuilder.cs
@@ -0,0 +1,13 @@
+//place this script in the Editor folder within Assets.
+using UnityEditor;
+ 
+ 
+//to be used on the command line:
+//$ Unity -quit -batchmode -executeMethod WebGLBuilder.build
+ 
+class WebGLBuilder {
+    static void build() {
+        string[] scenes = {"Assets/Scenes/SampleScene.unity"};
+        BuildPipeline.BuildPlayer(scenes, "Build/webgl", BuildTarget.WebGL, BuildOptions.None);
+    }
+}
